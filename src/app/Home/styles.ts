af635a006573864a13fca833522b32a3ed80087a
kauan <kauancavalcante8p@gmail.com>
2026-04-02 21:57:15 -0300

    organizando o sistema de pastas do projeto

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#bbb7b7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 80,
+        fontWeight: 900,
+        color: 'red',
+    },
+});
