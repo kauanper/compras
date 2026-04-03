80a74791aa2bea9d326d65ffedc70018764da619
kauan <kauancavalcante8p@gmail.com>
2026-04-03 03:56:03 -0300

    style: hearder em content

---
+++ src/app/components/Item/index.tsx
@@ -0,0 +1,12 @@
+import {View, TouchableOpacity, Text} from "react-native";
+Im
+
+import {styles} from "./styles";
+
+export function Item(){
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
